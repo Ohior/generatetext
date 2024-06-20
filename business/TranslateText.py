4b922d993e9b1743229d5b4e0c711d4fa24b553a
Ohiorenua <50307290+Ohior@users.noreply.github.com>
2024-06-20 02:54:32 +0100

    change translate engine

--- business/TranslateText.py
+++ business/TranslateText.py
@@ -1,28 +1,12 @@
-from googletrans import Translator
+import translators
 from models.GenTextModel import GenTextModel
 
 
 class TranslateText:
-    def __init__(self):
-        self.translator = Translator()
-
     def getTranslation(self, get_text: GenTextModel) -> str:
-        text = self.translator.translate(
-            text=get_text.text,
-            dest=get_text.to_lang.value,
-            src=get_text.from_lang.value
+        text = translators.translate_text(
+            query_text=get_text.text,
+            to_language=get_text.to_lang.value,
+            from_language=get_text.from_lang.value
         )
-        return text.text
-
-    def getTranslationMultiple(self, get_text: list[GenTextModel]) -> list[GenTextModel]:
-        translate_list: list[GenTextModel] = []
-        for i in get_text:
-            translate_list.append(self.translator.translate(
-                text=i.text,
-                dest=i.to_lang.value,
-                src=i.from_lang.value
-            ))
-        return translate_list
-
-    def getLanguage(self, text: str) -> str:
-        return self.translator.detect(text).lang
+        return text
